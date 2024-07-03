a5a21217bc0d3154c14cc23af46d463548f2e175
AnkurParmar <ankurparmar746@gmail.com>
2024-07-03 22:53:38 +0530

    Step 3 is fixed

--- stringCalculator.js
+++ stringCalculator.js
@@ -2,8 +2,10 @@ function add(numbers) {
     if (numbers === '') {
       return 0;
     }
-    const numArray = numbers.split(',').map(Number);
-    return numArray.reduce((sum, num) => sum + num, 0);
+
+  const numArray = numbers.split(/[\n,]/).map(Number);
+  
+  return numArray.reduce((sum, num) => sum + num, 0);
   }
   
   module.exports = add;
